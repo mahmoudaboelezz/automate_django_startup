74a7663da89c33d1d2db55d9961b17db767eff4d
Mahmoud Sobhy Aboelezz <73950001+mahmoudaboelezz@users.noreply.github.com>
2022-03-24 16:41:08 +0200

    Create startup.py

---
+++ startup.py
@@ -0,0 +1,38 @@
+import os
+import subprocess
+import webbrowser
+
+
+# to do 
+'''
+1-venv
+2-activate venv
+3-install django
+4-create project
+5-create app[option]
+6-migrate
+7-runserver
+8-website
+'''
+venv_name = str(input('Enter the name of venv : '))
+os.system(f'python -m venv {venv_name}')
+path = os.path.abspath(f'{venv_name}/Scripts')
+#venv_name/Script
+os.chdir(path)
+os.system('activate')
+os.system('pip install django')
+os.chdir(os.path.abspath('../..'))
+project_name = str(input('Enter the name of project : '))
+subprocess.run(f'django-admin startproject {project_name}')
+projectPath = os.path.abspath(f'{project_name}')
+os.chdir(projectPath)
+query = str(input('Would you like to create new app ? Y/n '))
+#yes / y / ye /e /s 
+if query.lower() in 'yes':
+    app_name = str(input('Enter the name of app : '))
+    os.system(f'python manage.py startapp {app_name}')
+else:
+    pass
+os.system('python manage.py migrate')
+webbrowser.open('http://127.0.0.1:8000/')
+os.system('python manage.py runserver')
